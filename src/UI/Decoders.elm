6893cbe72dbb85bc437285766675c4aaee236246
Chris Baker <electro@1939worldsfair.com>
2017-03-03 07:36:26 -0800

    moves reusable code into src and app-specific code to examples

---
+++ src/UI/Decoders.elm
@@ -0,0 +1,120 @@
+module UI.Decoders exposing (decodeModel, decodeLayoutInsert, decodeLayoutUpdate, decodeLayoutDelete)
+
+{-| Decoders for the UI types
+
+@docs decodeModel, decodeLayoutInsert, decodeLayoutUpdate, decodeLayoutDelete
+-}
+
+import UI.Types exposing (..)
+import Json.Decode as Json exposing (..)
+import Dict exposing (Dict)
+
+{-| Parses JSON and returns a UI
+-}
+decodeModel : Dict String (LayoutViewer pane paneMsg) -> (String -> Decoder pane) -> Decoder (Model pane paneMsg)
+decodeModel layouts decodePane =
+    (field "type" string) |> andThen (decodeRest layouts decodePane)
+
+
+decodeRest : Dict String (LayoutViewer pane paneMsg) -> (String -> Decoder pane) -> String -> Decoder (Model pane paneMsg)
+decodeRest layouts decodePane type_ =
+    oneOf
+        [ Json.map Layout (decodeLayout layouts decodePane type_)
+        , Json.map Choice (decodeChoice layouts decodePane type_)
+        , Json.map Pane (decodePane type_)
+        ]
+
+
+decodeLayout :
+    Dict String (LayoutViewer pane paneMsg)
+    -> (String -> Decoder pane)
+    -> String
+    -> Decoder (LayoutType pane paneMsg)
+decodeLayout layouts decodePane type_ =
+    case Dict.get type_ layouts of
+        Nothing ->
+            Json.fail "not a layout"
+
+        Just viewer ->
+            decodeLayoutChildren layouts decodePane viewer
+
+
+decodeLayoutChildren :
+    Dict String (LayoutViewer pane paneMsg)
+    -> (String -> Decoder pane)
+    -> LayoutViewer pane paneMsg
+    -> Decoder (LayoutType pane paneMsg)
+decodeLayoutChildren layouts decodePane viewer =
+    Json.map2 (LayoutType viewer)
+        (oneOf
+            [ field "subscriptions" <| decodeLayoutSubscriptions layouts decodePane
+            , Json.succeed
+                (Subscriptions Nothing Nothing Nothing)
+            ]
+        )
+        (Json.map List.reverse (field "children" (Json.keyValuePairs (lazy (\_ -> decodeModel layouts decodePane)))))
+
+
+decodeLayoutSubscriptions :
+    Dict String (LayoutViewer pane paneMsg)
+    -> (String -> Decoder pane)
+    -> Decoder (Subscriptions pane paneMsg)
+decodeLayoutSubscriptions layouts decodePane =
+    let
+        makeSub name =
+            ( name, decodeModel layouts decodePane )
+
+        decoder fieldName =
+            Json.maybe <| Json.map makeSub <| Json.field fieldName Json.string
+    in
+        Json.map3 Subscriptions
+            (decoder "insert")
+            (decoder "update")
+            (Json.maybe (Json.field "delete" Json.string))
+
+
+decodeChoice :
+    Dict String (LayoutViewer pane paneMsg)
+    -> (String -> Decoder pane)
+    -> String
+    -> Decoder (ChoiceType pane paneMsg)
+decodeChoice layouts decodePane type_ =
+    case type_ of
+        "Choice" ->
+            Json.map3 ChoiceType
+                (maybe (field "subscription" Json.string))
+                (field "initial" Json.string)
+                (field "children" (Json.keyValuePairs (lazy (\_ -> decodeModel layouts decodePane))))
+
+        _ ->
+            Json.fail "not a choice"
+
+
+{-| Parses JSON and returns a layout insert message
+-}
+decodeLayoutInsert :
+    Dict String (LayoutViewer pane paneMsg)
+    -> (String -> Decoder pane)
+    -> Decoder (LayoutMsgType pane paneMsg)
+decodeLayoutInsert layouts decodePane =
+    Json.map Insert
+        (Json.keyValuePairs <| decodeModel layouts decodePane)
+
+
+{-| Parses JSON and returns a layout update message
+-}
+decodeLayoutUpdate :
+    Dict String (LayoutViewer pane paneMsg)
+    -> (String -> Decoder pane)
+    -> Decoder (LayoutMsgType pane paneMsg)
+decodeLayoutUpdate layouts decodePane =
+    Json.map Update
+        (Json.keyValuePairs <| decodeModel layouts decodePane)
+
+
+{-| Parses JSON and returns a layout delete message
+-}
+decodeLayoutDelete : Decoder (LayoutMsgType pane paneMsg)
+decodeLayoutDelete =
+    Json.map Delete
+        (Json.list Json.string)
